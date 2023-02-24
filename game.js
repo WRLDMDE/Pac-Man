2b481385ce63b379f519a301df4459d124f22654
treyjaded <trevonjcharles@gmail.com>
2023-02-23 19:08:39 -0500

    Additional files & png/jpegs added.

---
+++ game.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+const pacmanFrames = document.getElementById("animation");
+const ghostFrames = document.getElementById("ghosts");
+
+let createRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+};
+
