52e1ceabcc114c86efdbb0fc13deb6d695469932
treyjaded <trevonjcharles@gmail.com>
2023-02-23 19:11:40 -0500

    More files added.

---
+++ pacman.js
@@ -0,0 +1,16 @@
+class Pacman {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = 4;
+        this.nextDirection = 4;
+        this.frameCount = 7;
+        this.currentFrame = 1;
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
+    }
+}
